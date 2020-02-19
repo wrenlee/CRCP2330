6b73a620cc495ad20b6bb8f9015c724d37e0a985
wrenlee <wrenthereader@gmail.com>
2020-02-18 22:44:41 -0600

    Feb 18 10:44pm Mult Prog

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -12,28 +12,38 @@
 //create variable of product @Prod
 //loop through until r1 is met to add r0 multiple times 
 //counter variable
-//(d = d + a)
 //call r2 @r2
 //set d to r2 d = r2
 
-@R0 //call up address 0
-D = M //data register now contains the value of the memory register
-
-//variables for counter (i) and sum
-@i
-M = 0
-@sum
-M = 0
+//variables for counter (i) and prod and n (r1)
+//@i
+//M = 0 //sets i to 0
+@prod
+M = 0 //sets prod which will hold product to 0
+@R1
+D = M //sets memory pull from r1 to data register
+@n
+M = D //puts data register into memory of n variable
+//now i = 0, prod = 0, n = r1
 
 (LOOP)
-@i
-D = M
+@R0
+D = M //calls up r0 to be added in the data register
+@prod
+M = D + M //adds r0 to prod value
+//@i
+//D = D + 1 //increment i
 @n
-D = D - M
-
+D = D - 1 //decreases n
+@LOOP
+D; JEQ //if data register (aka n) is equal to 0, jump to loop
 
 
 @prod
 D = M //sets memory of prod to the data register
+@R2 
+M = D //pushes product into the r2 slot
 
-0; JMP //terminate program through loop
+(END)
+@END
+0; JMP //terminate program through loop to end constantly
