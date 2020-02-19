23f40f07d4bec43469bc782a3fa4b49492c60eb2
wrenlee <wrenthereader@gmail.com>
2020-02-18 22:09:02 -0600

    Feb 18 10:08pm Mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//load ro @r0
+//create variable of product @Prod
+//loop through until r1 is met to add r0 multiple times 
+//counter variable
+//(d = d + a)
+//call r2 @r2
+//set d to r2 d = r2
+
+@R0 //call up address 0
+D = M //data register now contains the value of the memory register
+
+//variables for counter (i) and sum
+@i
+M = 0
+@sum
+M = 0
+
+(LOOP)
+@i
+D = M
+@n
+D = D - M
+
+
+
+@prod
+D = M //sets memory of prod to the data register
+
+0; JMP //terminate program through loop
