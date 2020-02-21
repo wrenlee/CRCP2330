3bb47078eeba3a5077b82b991045af2b6fbf9fdf
wrenlee <wrenthereader@gmail.com>
2020-02-20 22:54:13 -0600

    Feb 20 10:54pm Mult Prog

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -22,6 +22,12 @@
 @R2
 M = 0 //sets total to 0 just in case
 
+@R1
+D = M
+@LOOP
+D; JGT //tests to see if r1 has values in it or not
+
+(START)
 (LOOP)
 @R0
 D = M //calls up r0 to be added in the data register
@@ -32,7 +38,7 @@ D = M //sets memory value to d
 D = D - 1 //decreases r1 (n)
 M = D
 @END
-D; JEQ //go to end
+D; JEQ //go to end if r1 is 0
 @LOOP
 D; JGT //if data register (d) is greater than 0 (i > n), jump to loop it again
 
